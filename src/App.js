c566bc7450e761aaeb990a90fb6f0045754f7689
NurbolotOmorov <omorovnurbolot5@gmail.com>
2024-07-27 17:51:17 +0600

    test

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { useEffect, useState} from "react";
 import './App.css';
 
 function App() {
-  const [pokemons, setPokemons] = useState([]);
+    const [pokemons, setPokemons] = useState([]);
 
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-        .then(response => response.json())
-        .then(data => {
-          const fetches = data.results.map(result =>
-              fetch(result.url).then(res => res.json())
-          );
-          Promise.all(fetches).then(results => setPokemons(results));
-        });
-  }, []);
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=6')
+            .then(response => response.json())
+            .then(data => {
+                const fetches = data.results.map(result =>
+                    fetch(result.url).then(res => res.json())
+                );
+                Promise.all(fetches).then(results => setPokemons(results));
+            });
+    }, []);
+
+
 
   return (
     <div className="App">
@@ -21,8 +23,10 @@ function App() {
         <div className="card-container">
             {pokemons.map(pokemon => (
                 <div key={pokemon.id} className="card">
-                    <h2>{pokemon.name}</h2>
                     <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                    <div className="cardName">
+                        <h2>{pokemon.name}</h2>
+                    </div>
                 </div>
             ))}
         </div>
